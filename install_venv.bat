251444b460fc2ca2cf02fb6d6af3ffc2dd63f6ab
Kiyoung Im <imkiyoung@gmail.com>
2023-11-09 12:10:41 +0900

    added batch file for Python venv installation

---
+++ install_venv.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set current_dir=%homedrive%%homepath%
+
+echo Python venv environment needs 7 ~ 8 Gigabytes.   
+echo Default installation directory is %current_dir%, if you want to change enter new path. 
+
+set /p installation_dir=Enter new directory or press Enter key: 
+
+if "%installation_dir%" == "" (
+    set installation_dir=%current_dir%
+)
+
+echo %installation_dir% will be used. 
+pause
+
+if not exist "%installation_dir%\venv\Scripts" (
+    echo Creating venv...
+    python -m venv %installation_dir%\venv
+)
+
+call %installation_dir%\venv\Scripts\activate.bat
+
+echo Python packages will be installed. 
+pause
+
+pip install -r requirements.txt
+
+if errorlevel 1 (
+    echo.
+    echo Requirements installation failed. please remove venv folder and run install_venv.bat again.
+) else (
+    echo.
+    echo Requirements are installed successfully.
+)
+pause
